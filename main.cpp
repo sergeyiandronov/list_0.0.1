1bdf2e10415788d362f3bfd3e56b5340b4e13bb4
sergeyiandronov <sergeyiandronov@gmail.com>
2017-11-27 16:58:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <sstream>
+
 using namespace std;
 struct node_t {
     int value;
     node_t * next= nullptr;
 };
-
 
 
 void add_to_list(node_t *&listfirst,node_t *&listlast,int valueadd,int &size){
@@ -64,57 +64,60 @@ void reverse_list(node_t *&listfirst,node_t* &listlast,int size){
     }
     node_t * fakelistfirst=listlast;
     while(listlast!=listfirst) {
-      listlast->next = listfirst;
+        listlast->next = listfirst;
 
-      for (node_t *counter = listlast->next; counter!=listlast; counter = counter->next) {
-          listlast->next = counter;
-      }
+        for (node_t *counter = listlast->next; counter!=listlast; counter = counter->next) {
+            listlast->next = counter;
+        }
 
-      listlast=listlast->next;
-  }
+        listlast=listlast->next;
+    }
     listfirst=fakelistfirst;
     listlast->next= nullptr;
 
 }
+void deletelist(node_t* listfirst,node_t* listlast){
+    node_t* res;
+    for(node_t *counter=listfirst;counter!= nullptr;){
+        res=counter->next;
+        delete(counter);
+        counter=res;
+    }
+}
 int main() {
- node_t* listfirstptr= nullptr;
- node_t* listlastptr= nullptr;
- int size=0;
- char op;
+    node_t* listfirstptr= nullptr;
+    node_t* listlastptr= nullptr;
+    int size=0;
+    char op;
     cin>>op;
- while(op!='q'){
-     switch(op){
-         case '+':
-             int nw;
-             cin>>nw;
-             add_to_list(listfirstptr,listlastptr,nw,size);
-             cout_list(listfirstptr,size);
-             break;
-         case '-':
-             deletefirst(listfirstptr,listlastptr,size);
-             cout_list(listfirstptr,size);
-             break;
-         case '=':
-             cout_list(listfirstptr,size);
-             break;
-         case '/':
-             reverse_list(listfirstptr,listlastptr,size);
+    while(op!='q'){
+        switch(op){
+            case '+':
+                int nw;
+                cin>>nw;
+                add_to_list(listfirstptr,listlastptr,nw,size);
+                cout_list(listfirstptr,size);
 
-             cout_list(listfirstptr,size);
-             break;
+                break;
+            case '-':
+                deletefirst(listfirstptr,listlastptr,size);
+                cout_list(listfirstptr,size);
+                break;
+            case '=':
+                cout_list(listfirstptr,size);
+                break;
+            case '/':
+                reverse_list(listfirstptr,listlastptr,size);
 
-     }
-     cin>>op;
- }
+                cout_list(listfirstptr,size);
+                break;
+
+
+        }
+       cin>>op;
+    }
+    deletelist(listfirstptr,listlastptr);
 
 
 
 }
-
-
-
-
-
-
-
-
